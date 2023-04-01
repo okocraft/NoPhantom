05e1c5a8d50eadfd0ed005c2834c6f32cfc3fda3
Siroshun09 <siro.shun8@gmail.com>
2023-04-01 16:52:18 +0900

    build: remove junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,6 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
 tasks.compileJava {
@@ -27,8 +24,4 @@ tasks.processResources {
     filesMatching(listOf("plugin.yml")) {
         expand("projectVersion" to version)
     }
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
